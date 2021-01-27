f7639ad1986a2b94fa2deae3f49d2df420b980bf
Philane Sigwebela <kaizer.pk@gmail.com>
2021-01-28 00:31:26 +0200

    Added delivery method functionality

---
+++ client/src/app/core/services/checkout.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { IDeliveryMethod } from 'src/app/shared/models/DeliveryMethod';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CheckoutService {
+baseUrl = environment.apiUrl;
+constructor(private http: HttpClient) { }
+
+getDeliveryMethods(){
+  return this.http.get(this.baseUrl +"orders/deliveryMethods").pipe(
+    map((dm: IDeliveryMethod[]) => {
+      return dm.sort((a,b) => b.price - a.price);
+    })
+  );
+}
+}
